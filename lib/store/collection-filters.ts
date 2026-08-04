9a0b0b441791e81338b849fe7b5b6fd02fab6541
April Coffee Roasters ApS <camilleccomte@aprilcoffeeroastery.com>
2026-08-04 15:11:42 +0200

    Remove 'Title' from SIZE_OPTION_NAMES set

--- lib/store/collection-filters.ts
+++ lib/store/collection-filters.ts
@@ -75,7 +75,7 @@ export const filterSizeOptions = [
   "Organic",
 ] as const;
 
-const SIZE_OPTION_NAMES = new Set(["size", "taille", "Title"]);
+const SIZE_OPTION_NAMES = new Set(["size", "taille"]);
 
 function productHasAvailableSize(product: Product, sizes: string[]) {
   return product.variants.some(
