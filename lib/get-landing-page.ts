9cc56560db23ac647747e921c8e835cc0515e3de
April Coffee Roasters ApS <camilleccomte@aprilcoffeeroastery.com>
2026-08-28 10:31:38 +0200

    Change label 'Web Shop' to 'WEBSHOP'

--- lib/get-landing-page.ts
+++ lib/get-landing-page.ts
@@ -18,7 +18,7 @@ const LATEST_ADVICE_SLOT = 2;
 
 const defaultHeaderLinks: LandingLink[] = [
   { label: "LOCATIONS", href: "/shops", external: false },
-  { label: "Web Shop", href: "/collections/everything", external: false },
+  { label: "WEBSHOP", href: "/collections/everything", external: false },
   { label: "PROJECTS", href: "/projects", external: false },
   { label: "COFFEE & INFO", href: "/coffee-inf-recipes", external: false },
   {
